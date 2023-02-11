2bcc8529f33531d0319458610bb47e4eea96375f
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2023-02-11 17:01:22 +0000

    Add useAccountContext and custom AccountContextProvider

---
+++ src/hooks/AccountContext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useReducer} from "react";
+
+export const AccountContext = createContext();
+
+
+export const accountReducer = (state, action) => {
+    switch (action.type){
+        case('UPDATE'):
+            return{
+                account:action.payload
+            }
+        case('LOGOUT'):
+            return{
+                account:null
+            }
+        default:
+            return state
+    }
+}
+
+export const useAccountContext = () => {
+    const stateAndDispatch = useContext(AccountContext);
+
+    if(!stateAndDispatch){
+        throw Error("useAccountContext in scope!")
+    }
+    return stateAndDispatch
+}
+
+export const AccountContextProvider = ({children}) => {
+
+    const [state,dispatch]= useReducer(accountReducer, {account: null})
+  return(
+    <AccountContext.Provider value = {{...state, dispatch}}>
+        {children}
+    </AccountContext.Provider>
+  )  
+}
