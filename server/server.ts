089b1bc209aff792872ca5e685f84020f1c2c5e0
nyarosu <ronak.chatterjee65@gmail.com>
2023-02-11 12:13:35 +0000

    start server boilerplate

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,21 @@
-// server
+
+import express from 'express';
+import fs from 'fs'
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+
+// Security middleware
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}))
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+
+http.createServer({}, app).listen(4000, async () => {
+    console.log("Server is running")
+})
